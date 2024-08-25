9e39a2580dc1c98ede759c3ca652e6d5e3e47fad
Adrian Peñalver <adrian1414@hotmail.es>
2024-08-25 20:42:01 +0200

    validaciones y subida de tracks

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router();
-const {getItems, getItem, createItem} = require("../controllers/tracks");
+const { validatorCreateItem } = require("../validators/tracks")
+const { getItems, getItem, createItem } = require("../controllers/tracks");
 
 // https://localhost/tracks GET POST DELETE PUT
 
-router.get("/",getItems);
-router.post("/",createItem);
+router.get("/", getItems);
+router.post("/", validatorCreateItem, createItem);
 
 module.exports = router
